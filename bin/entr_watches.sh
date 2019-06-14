474bd01915941c57cf05e05559938271d7f21297
Podhajsky, Vojtech <vojtech.podhajsky@firma.seznam.cz>
2019-06-14 15:38:14 +0200

    entr: replace bash with sh

--- bin/entr_watches.sh
+++ bin/entr_watches.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-echo "$HOME/.Xresources" | entr bash -c 'xrdb "$HOME/.Xresources"; notify-send -a "entr" "reloaded Xresources"'
+#!/bin/sh
+echo "$HOME/.Xresources" | entr sh -c 'xrdb "$HOME/.Xresources"; notify-send -a "entr" "reloaded Xresources"'
